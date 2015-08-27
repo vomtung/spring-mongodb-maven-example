2f8db739d496f67e6050975bc22981adc1bd3189
hbthuy-tma <vominhtung@tma.com.vn>
2015-08-27 16:04:10 +0700

    sucess list all book from mongo db

--- spring-mongodb-maven-example/src/main/java/com/example/entity/Book.java
+++ spring-mongodb-maven-example/src/main/java/com/example/entity/Book.java
@@ -14,7 +14,13 @@ public class Book {
 
 	private String title;
 	
+	private String author;
+	
+	private Integer price;
+	
 	private String description;
+	
+	private String imageUrl;
 
 	public Book() {
 	}
@@ -46,6 +52,30 @@ public class Book {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
 	}
 
 	@Override
